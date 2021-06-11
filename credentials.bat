d11d1f17a76236349ceb08256eb511c024d07e85
FDL <77911760+FDLBOT@users.noreply.github.com>
2021-06-11 13:18:02 +0700

    Update credentials.bat

--- credentials.bat
+++ credentials.bat
@@ -6,7 +6,7 @@ del /f "C:\Users\Public\Desktop\Firefox.lnk" > out.txt 2>&1
 net config server /srvcomment:"Rebuild if this remote desktop is down!" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator @AyuMaudyEwe! /add >nul
+net user administrator @FDL12345 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -17,6 +17,6 @@ ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Login credentials:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed!"
-echo User: FreeRdpFDL
+echo User: administrator
 echo Pass: @FDL12345
 ping -n 10 127.0.0.1 >nul
